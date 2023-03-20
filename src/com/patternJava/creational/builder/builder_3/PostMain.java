68806c8ac72b742a69fde2a834a0e9baa5971ade
nazarchykur <nazarchykur@gmail.com>
2023-03-20 17:00:06 +0200

    triggered for verification

--- src/com/patternJava/creational/builder/builder_3/PostMain.java
+++ src/com/patternJava/creational/builder/builder_3/PostMain.java
@@ -34,5 +34,6 @@ public class PostMain {
 
         System.out.println("newPost = " + newPost);
         // newPost = Post{title='blog-post-builder', author='javadev', datePosed='20/03', numberOfWords=100, numberOfCharacters=1000}
+        
     }
 }
